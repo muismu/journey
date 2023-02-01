c1d5d941b09ee663abd5f172a41f6741582e46a3
muismu <muismu@users.noreply.github.com>
2023-02-01 06:02:28 +0000

    deploy: 767cccdc7d18f7afa302e4a456a76917c5a27905

--- en/js/bundle.8ec61e75a3e52261029f3db0f8c597590375f4488a696ab813cf0998346b14724996117029fcaf8da608895919f38d07ee9774ff2aefc38df2cb38de938267e3.js
+++ en/js/bundle.8ec61e75a3e52261029f3db0f8c597590375f4488a696ab813cf0998346b14724996117029fcaf8da608895919f38d07ee9774ff2aefc38df2cb38de938267e3.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = 'https://muismu.github.io/journey/';
+const baseURL = 'https://journey.muismu.com/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
